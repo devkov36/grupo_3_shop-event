6ab2108b39f65c776ae69df89c2934bc5b039320
lorecab12 <abogadalorenacabrera@gmail.com>
2022-05-22 02:13:04 -0300

    en proceso productCart

--- shop-event/app.js
+++ shop-event/app.js
@@ -9,6 +9,22 @@ app.listen ( port , () => {
     console.log (`Servidor corriendo en el puerto ${port}`);
 });
 
+app.get ( '/login', ( req , res) => { 
+    res.sendFile ( path.resolve ('./views/login.html'));
+});
+
+app.get ( '/productCart', ( req , res) => { 
+    res.sendFile ( path.resolve ('./views/productCart.html'));
+});
+
+app.get ( '/register', ( req , res) => { 
+    res.sendFile ( path.resolve ('./views/register.html'));
+});
+
+app.get ( '/productDetail', ( req , res) => { 
+    res.sendFile ( path.resolve ('./views/productDetail.html'));
+});
+
 app.get ( '/', ( req , res) => { 
     res.sendFile ( path.resolve ('./views/index.html'));
 });
