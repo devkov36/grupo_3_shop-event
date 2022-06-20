6de5e5265c984c9b5ffc2df59e5f093c302818f7
Ramon de Jesus Ortega <ramortega1594@gmail.com>
2022-06-20 18:49:45 -0500

    Agrega a rutas y controller lo correspondiente a editar evento

--- shop-event/src/routes/eventsRoutes.js
+++ shop-event/src/routes/eventsRoutes.js
@@ -7,7 +7,11 @@ router.get('/cart', eventsController.cart);
 
 router.get('/detail', eventsController.detail);
 
+// CREAR UN PRODUCTO
 router.get('/create', eventsController.create); 
+
+// EDITAR
+router.get('/edit/:id/', eventsController.edit);
 
 module.exports = router;
 
